15e86dd8f561e8c90b98b6b5bc5736b29159d758
IGrekov <4romangrekk@gmail.com>
2023-01-14 19:33:37 +0300

    залил

---
+++ src/pages/page1/index.js
@@ -0,0 +1,26 @@
+import Handlebars from 'handlebars';
+import tpl from 'bundle-text:./tpl.hbs';
+import './style.scss';
+
+Handlebars.registerPartial('page1', tpl);
+
+export default (props = {}) => {
+	return Handlebars.compile(tpl)(props);
+}
+
+
+// const authButton = document.querySelector("#authButton");
+
+window.addEventListener("click", (e) => {
+	if (e.target.id !== "authButton") {
+		return;
+	}
+	const loginInput = document.querySelector("#login");
+	console.log(loginInput.value);
+	if (loginInput.value.length === 0) {
+		return
+	}
+
+	window.location.href = "./#page10"
+})
+

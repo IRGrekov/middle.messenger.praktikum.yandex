274441817c500c4729e16823f257ec248f91cf7e
IGrekov <4romangrekk@gmail.com>
2023-01-15 14:24:33 +0300

    убрал crs

--- src/components/title/index.js
+++
@@ -1,9 +0,0 @@
-import Handlebars from 'handlebars';
-import tpl from 'bundle-text:./tpl.hbs';
-import './style.scss';
-
-Handlebars.registerPartial('title', tpl);
-
-export default () => {
-	return Handlebars.compile(tpl)({});
-}
